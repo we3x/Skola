c866b48f055b9c341f75d96aa149a2de6982b12a
Veljko Simic <veljko.simic98@gmail.com>
2016-05-12 09:41:20 +0200

    Basic function of BTree

---
+++ stablo/primer.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+typedef struct cvor {
+  int value;
+  struct cvor *levi;
+  struct cvor *desni;
+}BCvor;
+
+void insert_el (BCvor novi, BCvor **koren){
+  if (*koren == NULL){
+    *koren = novi;
+    novi -> levi = NULL;
+    novi -> desni = NULL;
+  } else {
+    if (novi -> value > *koren -> value){
+      insert_el(novi, &(*koren -> value));
+    }else if (novi -> value < *koren -> value){
+      insert_el(novi, &(*koren -> value))
+    }else
+      return ;
+  }
+}
+
+BCvor *search_el(BCvor *koren, int key){
+  if (koren == NULL){
+    return NULL;
+  }else{
+    if (koren -> value < key){
+      return search_el(koren -> levi, key)
+    }else if ( koren -> value > key){
+      return search_el(koren -> desni, key);
+    } else
+      return koren;
+  }
+}
+
+void print_tree(BCvor *koren){
+  if (koren == NULL){
+    return;
+  }
+  printf("%d ", koren -> value);
+  print_tree(koren -> levi);
+  print_tree(koren -> desni);
+}
