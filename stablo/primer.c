6bcb93a06dc34152e95d88c96f041e79e5f6d15d
Veljko Simic <veljko.simic98@gmail.com>
2016-05-14 13:15:15 +0200

    delete_node

--- stablo/primer.c
+++ stablo/primer.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct cvor {
   int value;
@@ -9,13 +10,13 @@ typedef struct cvor {
 void insert_el (BCvor novi, BCvor **koren){
   if (*koren == NULL){
     *koren = novi;
-    novi -> levi = NULL;
-    novi -> desni = NULL;
+    novi->levi = NULL;
+    novi->desni = NULL;
   } else {
-    if (novi -> value > *koren -> value){
-      insert_el(novi, &(*koren -> desni));
-    }else if (novi -> value < *koren -> value){
-      insert_el(novi, &(*koren -> levi))
+    if (novi->value > *koren->value){
+      insert_el(novi, &(*koren->desni));
+    }else if (novi->value < *koren->value){
+      insert_el(novi, &(*koren->levi));
     }else
       return ;
   }
@@ -25,10 +26,10 @@ BCvor *search_el(BCvor *koren, int key){
   if (koren == NULL){
     return NULL;
   }else{
-    if (koren -> value < key){
-      return search_el(koren -> levi, key)
-    }else if ( koren -> value > key){
-      return search_el(koren -> desni, key);
+    if (koren->value < key){
+      return search_el(koren->levi, key);
+    }else if ( koren->value > key){
+      return search_el(koren->desni, key);
     } else
       return koren;
   }
@@ -38,12 +39,12 @@ void print_tree(BCvor *koren){
   if (koren == NULL){
     return;
   }
-  printf("%d ", koren -> value);
-  print_tree(koren -> levi);
-  print_tree(koren -> desni);
+  printf("%d ", koren->value);
+  print_tree(koren->levi);
+  print_tree(koren->desni);
 }
 
-vodi delete_tree(BCvor *koren){
+void delete_tree(BCvor *koren){
   if( koren == NULL)
     return;
   delete_tree(koren->levi);
@@ -60,4 +61,32 @@ BCvor *min_node(BCvor *koren){
     return koren;
   else
     return child;
+}
+
+void delete_node(int key,BCvor **koren){
+  BCvor *min, *tmp;
+  if (*koren == NULL)
+    return;
+  if (key < (*koren)->value)
+    delete_node(key, &(*koren)->levi);
+  else if (key > (*koren)->value)
+    delete_node(key, &(*koren)->desni);
+  else {
+    if (((*koren)->levi == NULL) && ((*koren)->desni == NULL)){
+      free(*koren);
+      *koren = NULL;
+    }else if (((*koren)->levi != NULL) || ((*koren)->desni != NULL)){
+      tmp = *koren;
+      *koren = (*koren)->levi != NULL ? (*koren)->levi:(*koren)->desni;
+      free(tmp);
+    }else{
+      min = min_node((*koren)->desni);
+      (*koren)->value = min->value;
+      delete_node(min->value, &(*koren)->desni);
+    }
+  }
+}
+
+int main(){
+  return 0;
 }
